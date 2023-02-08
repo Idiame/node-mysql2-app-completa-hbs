33d866f23c5348b7e86ebf56254c0e9202a00044
German Mazku <germanmazku@gmail.com>
2023-02-08 13:20:44 +0100

    configuracion inicial terminada

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const { engine } = require('express-handlebars');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const linksRouter = require('./routes/links');
+const authenticationRouter = require('./routes/authentication');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', engine({
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'),'layouts'),
+  partialsDir: path.join(app.get('views'),'partials'),
+  extname: '.hbs', 
+  helpers: require('./lib/handlebars')
+}))
+
 app.set('view engine', 'hbs');
 
+//Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+//Global Variables
+app.use((req,res, next)=>{
+  next()
+})
+
+//Routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/links', linksRouter);
+app.use('/authentication', authenticationRouter)
 
+
+//Public
+app.use(express.static(path.join(__dirname, 'public')));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
