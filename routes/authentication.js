6f6a3bdaf17191a0f6087fec3e4ec1d6528c7b2d
German Mazku <germanmazku@gmail.com>
2023-02-16 10:10:32 +0100

    Password encrypted

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport')
 
 /* GET home page. */
 router.get('/signup',(req, res, next) =>{
   res.render('auth/signup');
 });
 
-router.post('/signup',(req, res, next) =>{
-  console.log(req.body)
-  res.send('Posteado');
-});
+router.post('/signup', passport.authenticate('local.signup', {
+  successRedirect: '/profile',
+  failureRedirect: '/signup',
+  failureFlash: true
+}))
+
+router.get('/profile', (req,res)=>{
+  res.send('THIS IS YOUR PROFILE')
+})
 
 module.exports = router;
